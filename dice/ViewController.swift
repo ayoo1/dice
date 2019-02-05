3f2b5e5d9331d3d9a6b07e8605e4505b3c88960d
Ayo Olabode <ayoo1@109d25pf0g0f8j7.student.cssd.ab.ca>
2019-02-05 14:10:38 -0700

    Initial Commit

---
+++ dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by Ayo Olabode on 2019-02-05.
+//  Copyright © 2019 Ayo Olabode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
